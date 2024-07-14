46075b59d25670d496ebf2c7c6fae2c907cd1754
daiikirii <macacarlome@gmail.com>
2024-07-14 16:01:42 -0300

    fedigital.mp3

---
+++ data.js
@@ -0,0 +1,30 @@
+let songs = [
+    {
+        path: 'canciones/intro.mp3',
+        cover: 'imagenes/intro.png'
+    },
+    {
+        path: 'canciones/mangata.mp3',
+        cover: 'imagenes/mangata.png'
+    },
+    {
+        path: 'canciones/last-maria.mp3',
+        cover: 'imagenes/fuensanta.png'
+    },
+    {
+        path: 'canciones/un-milagro.mp3',
+        cover: 'imagenes/un-milagro.png'
+    },
+    {
+        path: 'canciones/inri.mp3',
+        cover: 'imagenes/inri.png'
+    },
+    {
+        path: 'canciones/track3.mp3',
+        cover: 'imagenes/track-3.png'
+    },
+    {
+        path: 'canciones/toi-hecho.mp3',
+        cover: 'imagenes/toihecho.png'
+    },
+]
